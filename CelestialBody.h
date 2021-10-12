ee5b3124c740e80ca194992c04f82d0c1816af9e
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2021-10-11 20:29:32 -0400

    Update CelestialBody.h

--- CelestialBody.h
+++ CelestialBody.h
@@ -1,3 +1,11 @@
+/**
+ * CelestialBody.h - Defining the CelestialBody class
+ * 
+ * Author: Tim Robbins
+ * Date: 10/11/21
+ * Class: COMP.2040 - 202
+ * Instructor: Dr. Rykalova
+ */
 #ifndef CELESTIALBODY_H
 #define CELESTIALBODY_H
 #include <SFML/System.hpp>
@@ -11,6 +19,23 @@ public:
     void setWindowXSize(float size) { windowXSize = size; }
     void setWindowYSize(float size) { windowXSize = size; }
     void setModifiedPosition();
+    float getXPos() { return xPos; }
+    float getYPos() { return yPos; }
+    float getXVel() { return xVel; }
+    float getYVel() { return yVel; }
+    float getMass() { return mass; }
+    float getUniverseSize() { return universeSize; }
+    float getTempXAccel() { return tempXAccel; }
+    float getTempYAccel() { return tempYAccel; }
+    std::string getImageFile() { return imageFile; }
+    void setXPos(float xPos) { this->xPos = xPos; }
+    void setYPos(float yPos) { this->yPos = yPos; }
+    void setXVel(float xVel) { this->xVel = xVel; }
+    void setYVel(float yVel) { this->yVel = yVel; }
+    void setTempXAccel(float xPos) { this->tempXAccel = xPos; }
+    void setTempYAccel(float yPos) { this->tempYAccel = yPos; }
+
+
 
 private:
     float xPos;
@@ -18,11 +43,14 @@ private:
     float xVel;
     float yVel;
     float mass;
+    std::string imageFile;
     sf::Sprite sprite;
     sf::Texture texture;
     float universeSize;
     int windowXSize;
     int windowYSize;
+    float tempXAccel;
+    float tempYAccel;
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
