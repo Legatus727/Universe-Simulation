3349062d80fb1ef59841d353f5f540960514b93b
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2021-10-11 21:37:04 -0400

    Update Universe.h

--- Universe.h
+++ Universe.h
@@ -1,12 +1,3 @@
-/**
- * Universe.h - Defining the Universe class
- * 
- * Author: Tim Robbins
- * Date: 10/11/21
- * Class: COMP.2040 - 202
- * Instructor: Dr. Rykalova
- */
-
 #ifndef UNIVERSE_H
 #define UNIVERSE_H
 #include <vector>
@@ -21,7 +12,7 @@ public:
     friend std::istream& operator>>(std::istream& in, Universe& universe);
     void addPlanet(float xPos, float yPos, float xVel, float yVel, float mass, std::string imageFile);
     void step(double seconds);
-    void displayStats();
+    void writeStats(std::string fileName);
     void calculateStep(int planetIndex);
     int getNumPlanets() { return planets.size(); }
 
