16268b14c2c3c18baab777e90eea626f5e61e2a2
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2021-10-11 21:37:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,22 +9,53 @@
 int main(int argc, char* argv[]) {
 
     Universe universe;
+    float time = std::stof(argv[1]);
+	float timeChange = std::stof(argv[2]);
+    float elapsedTime = timeChange;
+
+    std::cout << "Accepted time and time change : " << time << " | " << timeChange << std::endl;
+    
     std::cin >> universe;
 
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Space.. The Final Frontier");
+    
+    sf::Image background;
+    if (!background.loadFromFile("starfield.jpg")) {
+        std::cout << "Failed to load background image" << std::endl;
+        exit(1);
+    }
+    sf::Texture texture;
+    texture.loadFromImage(background);
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
 
+
+    sf::RenderWindow window(sf::VideoMode(512, 512), "Space.. The Final Frontier");
+    
     while (window.isOpen()) {
         sf::Event event;
+
+        if (elapsedTime >= time) {
+            universe.writeStats("planets-output.txt");
+            window.close();
+        } 
+
+        for (int i = 0; i < universe.getNumPlanets(); i++) {
+            universe.calculateStep(i);
+        }
+
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
         window.clear();
+        window.draw(sprite);
         for (int i = 0; i < universe.getNumPlanets(); i++) {
             window.draw(universe[i]);
         }
         window.display();
+        universe.step(timeChange);
+        elapsedTime += timeChange;
     }
 
 
